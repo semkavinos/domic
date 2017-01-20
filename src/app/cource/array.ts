2a351111d138cfb73353301cd3f7bceea3dd93aa
semkavinos <semen970814@mail.ru>
2017-01-20 15:26:13 +0800

    #5 добавил класс

--- src/app/cource/array.ts
+++ src/app/cource/array.ts
@@ -1,13 +1,27 @@
-import { Cource } from './cource-sub';
-export const COURCE: Cource[] = [
-  {name: 'Mr. Nice'},
-  {name: 'Narco'},
-  {name: 'Bombasto'},
-  {name: 'Celeritas'},
-  {name: 'Magneta'},
-  {name: 'RubberMan'},
-  {name: 'Dynama'},
-  {name: 'Dr IQ'},
-  {name: 'Magma'},
-  {name: 'Tornado'}
-];
+import {DragulaService} from 'ng2-dragula/ng2-dragula';
+
+
+
+/*export class RepeatExample {
+  public many: Array<string> = ['The', 'possibilities', 'are', 'endless!'];
+  public many2: Array<string> = ['Explore', 'them'];
+
+  constructor(private dragulaService: DragulaService) {
+    dragulaService.dropModel.subscribe((value) => {
+      this.onDropModel(value.slice(1));
+    });
+    dragulaService.removeModel.subscribe((value) => {
+      this.onRemoveModel(value.slice(1));
+    });
+  }
+
+  private onDropModel(args) {
+    let [el, target, source] = args;
+    // do something else
+  }
+
+  private onRemoveModel(args) {
+    let [el, source] = args;
+    // do something else
+  }
+}*/
